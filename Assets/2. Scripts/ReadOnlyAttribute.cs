444fd52989c8373892a04309669954ba6082be4a
Kyle <sadoe3@gmail.com>
2021-11-09 16:48:36 +0900

    Add ReadOnlyAttribute script

---
+++ Assets/2. Scripts/ReadOnlyAttribute.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+}
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return base.GetPropertyHeight(property, label);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
