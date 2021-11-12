412ccdc192aa0f55551390fe11b4fd018ee44e99
sky <gksmf2787@naver.com>
2021-11-12 17:38:56 +0900

    키보드 탭키를 눌려서 아이템 화면창 뜨는것을 구현

--- Assets/2. Scripts/Managers/UIManager.cs
+++ Assets/2. Scripts/Managers/UIManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
+    private GameObject popupWeaponSwitch;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        popupWeaponSwitch = GetComponentInChildren<Image>().gameObject;
+        popupWeaponSwitch.SetActive(false);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            popupWeaponSwitch.SetActive(true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.Tab))
+        {
+            popupWeaponSwitch.SetActive(false);
+        }
     }
 }
