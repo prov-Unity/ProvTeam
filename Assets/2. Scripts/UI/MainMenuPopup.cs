94f4a286640a3b7435b585009f6e0556c9a8c5b7
sadoe3 <sadoe3@gmail.com>
2021-11-26 00:57:33 +0900

    update scripts related to save load

--- Assets/2. Scripts/UI/MainMenuPopup.cs
+++ Assets/2. Scripts/UI/MainMenuPopup.cs
@@ -5,10 +5,11 @@ public class MainMenuPopup : MonoBehaviour
     public void OnClickStart() {
         // do something to start the new game
         MySceneManager.instance.LoadScene("Tutorial");
+        MySceneManager.instance.isInitial = true;
     }
 
     public void OnClickLoad() {
         // do something to load main menu
-
+        MySceneManager.instance.isInitial = false;
     }
 }
